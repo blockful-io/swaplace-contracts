14e185d23955f3de8203f34f400fbb4a86f0c4c1
GUILHERME NEVES <asguilift@hotmail.com>
2023-05-09 11:13:52 -0300

    add public fetcher for total supply

--- contracts/mock/MockERC721.sol
+++ contracts/mock/MockERC721.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.9;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract MockERC721 is ERC721 {
-    uint256 totalSupply;
+    uint256 public totalSupply;
 
     constructor(
         string memory name,
