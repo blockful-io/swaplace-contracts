ab61398e74b971792fe20f101394375a99feefb4
GUILHERME NEVES <asguilift@hotmail.com>
2023-05-28 11:06:47 -0300

    change: prepare for new architecture

--- contracts/Swaplace.sol
+++ contracts/Swaplace.sol
@@ -69,46 +69,27 @@ contract Swaplace is ISwaplace, IERC165, ReentrancyGuard {
     uint256 public tradeId = 0;
 
     mapping(uint256 => Trade) private trades;
-    mapping(address => uint256[]) private tradesOwned;
-
-    /// Executions - Will be moved to a new contract, then imported here
-
-    uint256 public index = 0;
-    mapping(bytes32 => bytes) private executions;
-
-    function registerExecution(bytes calldata data) public {
-        index++;
-        bytes32 id = keccak256(abi.encodePacked(index, msg.sender, data));
-        executions[id] = data;
-    }
-
-    function getExecutionId(
-        uint256 _index,
-        address _addr,
-        bytes memory data
-    ) public pure returns (bytes32) {
-        return keccak256(abi.encodePacked(_index, _addr, data));
-    }
-
-    function getExecutions(bytes32 id) public view returns (bytes memory) {
-        return executions[id];
-    }
-
-    /// Trades
 
     function createTrade(
         Trade calldata trade
     ) public nonReentrant returns (uint256) {
-        valid(trade.owner, trade.expiry);
+        if (trade.owner == address(0) || trade.owner != msg.sender) {
+            revert CannotBeZeroAddress();
+        }
+
+        if (trade.expiry < 1 days) {
+            revert CannotBeLesserThanOneDay(trade.expiry);
+        }
 
         unchecked {
             tradeId++;
         }
 
         trades[tradeId] = trade;
-        trades[tradeId].expiry += block.timestamp;
 
-        tradesOwned[msg.sender].push(tradeId);
+        unchecked {
+            trades[tradeId].expiry += block.timestamp;
+        }
 
         return tradeId;
     }
@@ -123,21 +104,11 @@ contract Swaplace is ISwaplace, IERC165, ReentrancyGuard {
         Asset[] memory assets = trade.asking;
 
         for (uint256 i = 0; i < assets.length; ) {
-            if (assets[i].assetType == AssetType.FUNCTION_CALL) {
-                (bool success, bytes memory reason) = assets[i].addr.call(
-                    executions[bytes32(assets[i].amountOrIdOrCall)]
-                );
-
-                if (!success) {
-                    revert FunctionCallFailedWithReason(reason);
-                }
-            } else {
-                IUniversalTransfer(assets[i].addr).transferFrom(
-                    msg.sender,
-                    trade.owner,
-                    assets[i].amountOrIdOrCall
-                );
-            }
+            IUniversalTransfer(assets[i].addr).transferFrom(
+                msg.sender,
+                trade.owner,
+                assets[i].amountOrIdOrCall
+            );
             unchecked {
                 i++;
             }
@@ -146,21 +117,11 @@ contract Swaplace is ISwaplace, IERC165, ReentrancyGuard {
         assets = trade.assets;
 
         for (uint256 i = 0; i < assets.length; ) {
-            if (assets[i].assetType == AssetType.FUNCTION_CALL) {
-                (bool success, bytes memory reason) = assets[i].addr.call(
-                    executions[bytes32(assets[i].amountOrIdOrCall)]
-                );
-
-                if (!success) {
-                    revert FunctionCallFailedWithReason(reason);
-                }
-            } else {
-                IUniversalTransfer(assets[i].addr).transferFrom(
-                    trade.owner,
-                    msg.sender,
-                    assets[i].amountOrIdOrCall
-                );
-            }
+            IUniversalTransfer(assets[i].addr).transferFrom(
+                trade.owner,
+                msg.sender,
+                assets[i].amountOrIdOrCall
+            );
             unchecked {
                 i++;
             }
@@ -183,120 +144,8 @@ contract Swaplace is ISwaplace, IERC165, ReentrancyGuard {
         trades[_tradeId].expiry = 0;
     }
 
-    function valid(address owner, uint256 expiry) internal view {
-        if (owner == address(0)) {
-            revert CannotBeZeroAddress();
-        }
-        if (owner != msg.sender) {
-            revert OwnerMustBeMsgSender();
-        }
-        // Required the expiration date to be at least 1 day in the future
-        if (expiry < 1 days) {
-            revert CannotBeLesserThanOneDay(expiry);
-        }
-    }
-
-    function valid(uint256 expiry) internal pure {
-        // Required the expiration date to be at least 1 day in the future
-        if (expiry < 1 days) {
-            revert CannotBeLesserThanOneDay(expiry);
-        }
-    }
-
-    function makeAsset(
-        address addr,
-        uint256 amountOrIdOrCall,
-        AssetType assetType
-    ) public pure returns (Asset memory) {
-        if (
-            assetType != AssetType.ERC20 &&
-            assetType != AssetType.ERC721 &&
-            assetType != AssetType.FUNCTION_CALL
-        ) {
-            revert InvalidAssetType(uint256(assetType));
-        }
-
-        if (
-            (assetType == AssetType.ERC20 && amountOrIdOrCall == 0) ||
-            (assetType == AssetType.FUNCTION_CALL && amountOrIdOrCall == 0)
-        ) {
-            revert CannotBeZeroForAmountOrCall();
-        }
-
-        return Asset(addr, amountOrIdOrCall, assetType);
-    }
-
-    function makeTrade(
-        address owner,
-        uint256 expiry,
-        Asset[] memory assets,
-        Asset[] memory asking
-    ) public pure returns (Trade memory) {
-        valid(expiry);
-
-        if (owner == address(0)) {
-            revert CannotBeZeroAddress();
-        }
-
-        if (assets.length == 0 || asking.length == 0) {
-            revert CannotBeEmptyAssets();
-        }
-
-        return Trade(owner, expiry, assets, asking);
-    }
-
-    function composeTrade(
-        address owner,
-        uint256 expiry,
-        address[] memory addrs,
-        uint256[] memory amountsOrIdsOrCalls,
-        AssetType[] memory assetTypes,
-        uint256 indexFlipToAsking
-    ) public pure returns (Trade memory) {
-        if (
-            addrs.length != amountsOrIdsOrCalls.length ||
-            addrs.length != assetTypes.length
-        ) {
-            revert LengthMismatchWhenComposing(
-                addrs.length,
-                amountsOrIdsOrCalls.length,
-                assetTypes.length
-            );
-        }
-
-        Asset[] memory assets = new Asset[](indexFlipToAsking);
-        for (uint256 i = 0; i < indexFlipToAsking; ) {
-            assets[i] = makeAsset(
-                addrs[i],
-                amountsOrIdsOrCalls[i],
-                assetTypes[i]
-            );
-            unchecked {
-                i++;
-            }
-        }
-
-        Asset[] memory asking = new Asset[](addrs.length - indexFlipToAsking);
-        for (uint256 i = indexFlipToAsking; i < addrs.length; ) {
-            asking[i - indexFlipToAsking] = makeAsset(
-                addrs[i],
-                amountsOrIdsOrCalls[i],
-                assetTypes[i]
-            );
-            unchecked {
-                i++;
-            }
-        }
-
-        return makeTrade(owner, expiry, assets, asking);
-    }
-
     function getTrade(uint256 id) public view returns (Trade memory) {
         return trades[id];
-    }
-
-    function getTradesBy(address addr) public view returns (uint256[] memory) {
-        return tradesOwned[addr];
     }
 
     function supportsInterface(
