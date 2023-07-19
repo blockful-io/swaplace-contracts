c5e17f35e42d0f1c988269cbb9efcb1b9658c94c
Axis, the high wizard <asguilift@hotmail.com>
2023-07-19 14:46:53 -0300

    fix, comment: swap factory

--- contracts/SwapFactory.sol
+++ contracts/SwapFactory.sol
@@ -3,10 +3,17 @@ pragma solidity ^0.8.17;
 
 import {ISwap} from "./interfaces/ISwap.sol";
 
-error InvalidExpiryDate(uint256 timestamp);
-error InvalidMismatchingLengths(uint256 addr, uint256 amountOrCallOrId);
-
+/**
+ * @title SwapFactory
+ * @author 0xneves | @blockful_io
+ * @notice Factory contract to assist when creating swaps.
+ */
 abstract contract SwapFactory is ISwap {
+    /**
+     * @notice Create a new asset.
+     * @param addr - The address of the asset.
+     * @param amountOrCallOrId - The uint256 corresponding type of call to the address.
+     */
     function makeAsset(
         address addr,
         uint256 amountOrCallOrId
@@ -14,6 +21,11 @@ abstract contract SwapFactory is ISwap {
         return Asset(addr, amountOrCallOrId);
     }
 
+    /**
+     * @notice Create a new base swap.
+     * @param biding - The assets that the creator wants to give.
+     * @param asking - The assets that the creator wants to receive.
+     */
     function makeBaseSwap(
         Asset[] memory biding,
         Asset[] memory asking
@@ -21,6 +33,12 @@ abstract contract SwapFactory is ISwap {
         return BaseSwap(biding, asking);
     }
 
+    /**
+     * @notice Create a new private swap.
+     * @param allowed - The address that is allowed to accept the swap.
+     * @param biding - The assets that the creator wants to give.
+     * @param asking - The assets that the creator wants to receive.
+     */
     function makePvtSwap(
         address allowed,
         Asset[] memory biding,
@@ -29,28 +47,33 @@ abstract contract SwapFactory is ISwap {
         return PvtSwap(allowed, biding, asking);
     }
 
+    /**
+     * @notice Create a new timed swap.
+     * @param expiry - The expiry date of the swap.
+     * @param biding - The assets that the creator wants to give.
+     * @param asking - The assets that the creator wants to receive.
+     */
     function makeTimedSwap(
         uint256 expiry,
         Asset[] memory biding,
         Asset[] memory asking
     ) public pure returns (TimedSwap memory) {
-        if (expiry == 0) {
-            revert InvalidExpiryDate(expiry);
-        }
-
         return TimedSwap(expiry, biding, asking);
     }
 
+    /**
+     * @notice Create a new timed private swap.
+     * @param allowed - The address that is allowed to accept the swap.
+     * @param expiry - The expiry date of the swap.
+     * @param biding - The assets that the creator wants to give.
+     * @param asking - The assets that the creator wants to receive.
+     */
     function makeTimedPvtSwap(
         address allowed,
         uint256 expiry,
         Asset[] memory biding,
         Asset[] memory asking
     ) public pure returns (TimedPvtSwap memory) {
-        if (expiry == 0) {
-            revert InvalidExpiryDate(expiry);
-        }
-
         return TimedPvtSwap(allowed, expiry, biding, asking);
     }
 }
