1400cf6ba29ad48e417d575a9fc07db35e81a283
Guilherme Costa Neves <axis@Guilhermes-MacBook-Air.local>
2023-10-31 06:55:04 -0300

    fix: revoke address zero check from createSwap

--- contracts/Swaplace.sol
+++ contracts/Swaplace.sol
@@ -31,7 +31,7 @@ contract Swaplace is SwapFactory, DataPalace, ISwaplace, IERC165 {
     mapping(uint256 => Swap) private swaps;
 
     function createSwap(Swap calldata swap) public returns (uint256) {
-        if (swap.owner == address(0) || swap.owner != msg.sender) {
+        if (swap.owner != msg.sender) {
             revert InvalidAddress(swap.owner);
         }
 
