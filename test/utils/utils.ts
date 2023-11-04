c74c85c291c15cae62692b4f1552769f5e8f7932
Guilherme Costa Neves <axis@Guilhermes-MacBook-Air.local>
2023-11-04 00:34:07 -0300

    feat: added method for block timestamp
    - added a method for getting block timestamp in `utils.ts`
    - changed the timestamp approach from the tests into calling the utils lib
    
    close #42

--- test/utils/utils.ts
+++ test/utils/utils.ts
@@ -7,6 +7,18 @@ export function isValidAddr(addr: string): boolean {
 	return !/^0x[a-fA-F0-9]{40}$/gm.test(addr);
 }
 
+/**
+ * @dev Get the current `block.timestamp` in seconds from the current
+ * selected network.
+ *
+ * Use `-- network localhost` to run the tests in a local network.
+ * Networks should be in `hardhat.config.ts` file or via command line.
+ */
+export async function blocktimestamp(): Promise<number> {
+	return (await ethers.provider.getBlock("latest")).timestamp;
+}
+
 module.exports = {
 	isValidAddr,
+	blocktimestamp,
 };
