16ae8c824cb6c8c77cbeba8b3e48b11518259a90
Matheus Iotti <matheus.iotti98@gmail.com>
2023-11-05 10:56:29 -0300

    feat: set default provider for EVM networks

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -9,6 +9,10 @@ import "hardhat-gas-reporter";
 import dotenv from "dotenv";
 dotenv.config();
 
+const DEPLOYER_PRIVATE_KEY = process.env.DEPLOYER_PRIVATE_KEY || "";
+const TRADE_CREATOR_PRIVATE_KEY = process.env.TRADE_CREATOR_PRIVATE_KEY || "";
+const TRADE_ACCEPTEE_PRIVATE_KEY = process.env.TRADE_ACCEPTEE_PRIVATE_KEY || "";
+
 // const { MUMBAI, SEPOLIA, PKEY_DEPLOYER, POLYSCAN } = process.env;
 
 const config: HardhatUserConfig = {
@@ -35,6 +39,37 @@ const config: HardhatUserConfig = {
 		// 	url: `${SEPOLIA}`,
 		// 	accounts: [`${PKEY_DEPLOYER}`],
 		// },
+		
+	BNB: {
+		url: process.env.BNB,
+		accounts: [DEPLOYER_PRIVATE_KEY, TRADE_CREATOR_PRIVATE_KEY, TRADE_ACCEPTEE_PRIVATE_KEY],
+	},
+
+	MATIC: {
+		url: process.env.MATIC,
+		accounts: [DEPLOYER_PRIVATE_KEY, TRADE_CREATOR_PRIVATE_KEY, TRADE_ACCEPTEE_PRIVATE_KEY],
+	},
+
+	ETH: {
+		url: process.env.ETH,
+		accounts: [DEPLOYER_PRIVATE_KEY, TRADE_CREATOR_PRIVATE_KEY, TRADE_ACCEPTEE_PRIVATE_KEY],
+	},
+
+	AVAX: {
+		url: process.env.AVAX,
+		accounts: [DEPLOYER_PRIVATE_KEY, TRADE_CREATOR_PRIVATE_KEY, TRADE_ACCEPTEE_PRIVATE_KEY],
+	},
+
+	FTM: {
+		url: process.env.FTM,
+		accounts: [DEPLOYER_PRIVATE_KEY, TRADE_CREATOR_PRIVATE_KEY, TRADE_ACCEPTEE_PRIVATE_KEY],
+	},
+
+	ASTR: {
+		url: process.env.ASTR,
+		accounts: [DEPLOYER_PRIVATE_KEY, TRADE_CREATOR_PRIVATE_KEY, TRADE_ACCEPTEE_PRIVATE_KEY],
+	},
+
 	},
 };
 
