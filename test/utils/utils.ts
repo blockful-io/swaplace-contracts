65567025aa39d664748c07414a646f62cc3116cc
0xneves <asguilift@hotmail.com>
2023-11-08 17:03:03 -0300

    feat: generalized deploy script

--- test/utils/utils.ts
+++ test/utils/utils.ts
@@ -18,6 +18,33 @@ export async function blocktimestamp(): Promise<any> {
 	return (await ethers.provider.getBlock("latest")).timestamp;
 }
 
+/**
+ * @dev Deploys a contract and returns the contract instance.
+ * The signer is optional, if not provided, the first signer will be used.
+ * @param contractName The contract name to deploy.
+ * @param signer The signer to use.
+ */
+export async function deploy(contractName: string, signer?: any) {
+	// Get Contract Factory for contractName
+	const ContractFactory = await ethers.getContractFactory(contractName, signer);
+
+	// Deploy the Contract
+	const Contract = await ContractFactory.deploy();
+
+	// Wait for Contract to be deployed
+	const tx = await Contract.deployed();
+
+	// Log Contract address, the Tx then return the Contract instance
+	console.log(
+		"\nContract %s \nDeployed to %s \nAt Tx %s",
+		contractName,
+		Contract.address,
+		tx
+	);
+
+	return Contract;
+}
+
 module.exports = {
 	isValidAddr,
 	blocktimestamp,
