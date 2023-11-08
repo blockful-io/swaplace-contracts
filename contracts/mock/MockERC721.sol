8cde1ab2fc3a4fc11fa8063e4f41b46f1f96678c
0xneves <asguilift@hotmail.com>
2023-11-08 19:07:46 -0300

    update: mocks don't need constructor arguments

--- contracts/mock/MockERC721.sol
+++ contracts/mock/MockERC721.sol
@@ -6,10 +6,7 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 contract MockERC721 is ERC721 {
     uint256 public totalSupply;
 
-    constructor(
-        string memory name,
-        string memory symbol
-    ) ERC721(name, symbol) {}
+    constructor() ERC721("MockERC721", "ERC721") {}
 
     function mintTo(address to) public {
         totalSupply++;
