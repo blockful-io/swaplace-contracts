7ab82045b4eb294fe6b3eda5b7f6f46bb840ee11
0xneves <asguilift@hotmail.com>
2023-11-08 19:07:30 -0300

    update: deploy script is imported
    - made more easy to use the deploy script and also re-use on other scripts and tests

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,21 +1,12 @@
 import { ethers } from "hardhat";
+import { deploy } from "./utils/utils";
 
 async function main() {
-	// Get signer
+	// Get the first account from the list of accounts
 	const [signer] = await ethers.getSigners();
 
-	// Get Swaplace Factory
-	const SwaplaceFactory = await ethers.getContractFactory("Swaplace", signer);
-
-	// Deploy Swaplace
-	const Swaplace = await SwaplaceFactory.deploy();
-
-	// Wait for Swaplace to be deployed
-	await Swaplace.deployed();
-
-	// Log Swaplace address and Tx with polygon scan link
-	console.log("Swaplace deployed to:", Swaplace.address);
-	console.log("https://polygonscan.com/tx/" + Swaplace.deployTransaction.hash);
+	// Deploy in the currrent network and return the Swaplace instance
+	const Swaplace = await deploy("Swaplace", signer);
 }
 
 main().catch((error) => {
