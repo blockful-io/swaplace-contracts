b02d8cb0921710714e8bf676570d8520ee95c751
Deepu <deepuharry02@gmail.com>
2023-11-24 01:31:28 +0530

    minting ERC20

--- scripts/mock.ts
+++ scripts/mock.ts
@@ -13,7 +13,7 @@ export async function main() {
     // Log Contract address, the Tx then return the Contract instance of MockERC20
 	console.log(
 		"\nContract %s \nDeployed to %s \nAt Tx %s",
-		"Swaplace",
+		"MockERC20",
 		MockERC20.address,
 		MockERC20.deployTransaction.hash
 	);
@@ -21,10 +21,16 @@ export async function main() {
     // Log Contract address, the Tx then return the Contract instance of MockERC721
 	console.log(
 		"\nContract %s \nDeployed to %s \nAt Tx %s",
-		"Swaplace",
+		"MockERC721",
 		MockERC721.address,
 		MockERC721.deployTransaction.hash
 	);
+
+    //Mint ERC20 1000000 tokens
+    const amountToMint = ethers.utils.parseUnits('1000000', 18);
+    const mintTo=await MockERC20.mintTo(signer,amountToMint);
+    console.log("ERC20 balance of the signer:"+await MockERC20.balanceOf(signer));
+
 }
 
 main().catch((error) => {
