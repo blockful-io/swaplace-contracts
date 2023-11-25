1ba3c03b1dd32e1a38447e1cf3e0e1526dd36140
Deepu <deepuharry02@gmail.com>
2023-11-25 18:13:41 +0530

    feat: deploy mock contracts #100: changed the spender address to contract's address

--- scripts/mockERC.ts
+++ scripts/mockERC.ts
@@ -1,9 +1,9 @@
 import {ethers} from "hardhat";
-import { deploy } from "../test/utils/utils";
+import { deploy } from "./utils/utils";
 
 export async function main() {
     // Get the first account from the list of accounts
-	const [signer,spender] = await ethers.getSigners();
+	const [signer] = await ethers.getSigners();
 
     // Deploy in the currrent network and return the contract instance
 	const MockERC20 = await deploy("MockERC20", signer);
@@ -11,7 +11,7 @@ export async function main() {
 
     // Log Contract address, the Tx then return the Contract instance of MockERC20
 	console.log(
-		"\nContract %s \nDeployed to %s \nAt Tx %s",
+		"\nDEPLOY:\nContract %s \nDeployed to %s \nAt Tx %s",
 		"MockERC20",
 		MockERC20.address,
 		MockERC20.deployTransaction.hash
@@ -33,7 +33,7 @@ export async function main() {
     await mintTo.wait();
 
     //Log the signer's ERC20 balance in terms of wei after minting 69 tokens 
-    console.log("\nERC20 balance of the signer:"+await MockERC20.balanceOf(signer.address));
+    console.log("\nMINT:\nERC20 balance of the signer:"+await MockERC20.balanceOf(signer.address));
 
     //Mint ERC721 token
     const tokenId=1;
@@ -43,26 +43,26 @@ export async function main() {
     await mintTo.wait();
 
     //Log the owner of the tokenId=1
-    console.log("\nOwner of tokeId 1:"+await MockERC721.ownerOf(tokenId));
+    console.log("Owner of tokeId 1:"+await MockERC721.ownerOf(tokenId));
 
     //approve 10 ERC20 tokens to the spender
     const amountToApprove = ethers.utils.parseUnits('10');
-    let approve=await MockERC20.approve(spender.address,amountToApprove);
+    let approve=await MockERC20.approve(MockERC20.address,amountToApprove);
 
     // Wait for the transaction to be mined
     await approve.wait();
 
     //log the allowance of the spender
-    console.log("\nAllowance of the spender:"+await MockERC20.allowance(signer.address,spender.address));
+    console.log("\nALLOWANCE\nAllowance of the contract:"+MockERC20.address+" allowed by the signer:"+await MockERC20.allowance(signer.address,MockERC20.address));
 
     //approve token ID 1 to spender
-    approve=await MockERC721.approve(spender.address,tokenId);
+    approve=await MockERC721.approve(MockERC721.address,tokenId);
 
     // Wait for the transaction to be mined
     await approve.wait();
 
     //log the approved address of token ID 1
-    console.log("\nApproved address of token ID 1:"+await MockERC721.getApproved(tokenId));
+    console.log("Approved address of token ID 1:"+await MockERC721.getApproved(tokenId));
 }
 
 main().catch((error) => {
