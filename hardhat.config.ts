dde30f6d09356b2f0eb16c3ae9cc5beccc0259d1
dipanshuhappy <dipanshuhappy@gmail.com>
2023-11-28 21:32:04 +0530

    feat(Docs): Set Up Docs using solidity-docgen

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,5 +1,6 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import 'solidity-docgen';
 import dotenv from "dotenv";
 dotenv.config();
 
@@ -62,6 +63,11 @@ const config: HardhatUserConfig = {
 			accounts: [`${DEPLOYER_PRIVATE_KEY}`],
 		},
 	},
+	docgen: {
+		outputDir: 'docs',
+		pages: "files",
+
+	}
 };
 
 export default config;
