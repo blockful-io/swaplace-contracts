50d191452c01a22479595eed6b144517a0abbe80
Rafael de Souza Sanches <rafaeldss12@gmail.com>
2024-06-21 11:17:44 -0300

    fix: removed sensitive info

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -7,7 +7,7 @@ dotenv.config();
 // Using a hardcoded solution to avoid GitHub actions issues
 const DEPLOYER_PRIVATE_KEY =
   process.env.DEPLOYER_PRIVATE_KEY ||
-  "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80";
+  "";
 
 const config: HardhatUserConfig = {
   solidity: {
